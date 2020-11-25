48e2cdb11f393e818a7a4d756b970f891cbfd716
Vivek Zagade <vivekzagade@localhost.localdomain>
2020-11-25 13:48:39 +0530

    1.0.3 Added Functionality: POST method

--- messenger/src/main/java/krishagni/messenger/resources/MessageResource.java
+++ messenger/src/main/java/krishagni/messenger/resources/MessageResource.java
@@ -2,7 +2,9 @@ package krishagni.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,9 +19,17 @@ public class MessageResource {
 	MessageService messageService = new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getAllMessages() {
 		return messageService.getAllMessages();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message) 
+	{
+		return messageService.addMessage(message);
 	}
 	
 	@GET
@@ -32,7 +42,7 @@ public class MessageResource {
 	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") long id)
 	{
 		return messageService.getMessage(id);
