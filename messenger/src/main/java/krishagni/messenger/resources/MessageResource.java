5fd1f783bd18dd87a4a53d80ab2512821f64ac61
Vivek Zagade <vivekzagade@localhost.localdomain>
2020-11-25 17:51:53 +0530

    1.0.5 Added Functionality: Querying Resources using bean param

--- messenger/src/main/java/krishagni/messenger/resources/MessageResource.java
+++ messenger/src/main/java/krishagni/messenger/resources/MessageResource.java
@@ -2,6 +2,7 @@ package krishagni.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -10,10 +11,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import krishagni.messenger.model.Message;
+import krishagni.messenger.resources.beans.MessageFilterBeans;
 import krishagni.messenger.service.MessageService;
 
 @Path("/messages")
@@ -24,16 +25,14 @@ public class MessageResource {
 	MessageService messageService = new MessageService();
 	
 	@GET
-	public List<Message> getAllMessages(@QueryParam("year") int year,
-										@QueryParam("start") int start,
-										@QueryParam("size") int size) {
-		if(year > 0)
+	public List<Message> getAllMessages(@BeanParam MessageFilterBeans filterBean ) {
+		if(filterBean.getYear() > 0)
 		{
-			return messageService.getAllMessageForYear(year);
+			return messageService.getAllMessageForYear(filterBean.getYear());
 		}
-		if(size > 0 && start > 0)
+		if(filterBean.getSize() > 0 && filterBean.getStart() > 0)
 		{
-			return messageService.getAllMessagePaginated(start, size);
+			return messageService.getAllMessagePaginated(filterBean.getStart(), filterBean.getSize());
 		}
 		return messageService.getAllMessages();
 	}
